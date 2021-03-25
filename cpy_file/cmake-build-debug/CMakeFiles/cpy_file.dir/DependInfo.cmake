b5d8e7043f1b5564227831937c63a5a0fb1a4d66
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-03-25 07:41:57 +0200

    Add files via upload

---
+++ cpy_file/cmake-build-debug/CMakeFiles/cpy_file.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ADMIN/CLionProjects/cpy_file/main.c" "C:/Users/ADMIN/CLionProjects/cpy_file/cmake-build-debug/CMakeFiles/cpy_file.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
