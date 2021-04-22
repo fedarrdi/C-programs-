a4b6ce762e5b0494cde2e0fc424202a227a865bc
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-04-22 12:55:49 +0300

    Add files via upload

---
+++ tree_cmp/cmake-build-debug/CMakeFiles/tree_cmp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tree_cmp.dir/main.c.obj"
+  "tree_cmp.exe"
+  "tree_cmp.exe.manifest"
+  "tree_cmp.lib"
+  "tree_cmp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tree_cmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
