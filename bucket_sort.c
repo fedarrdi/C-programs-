8298f1398627bb91c295c549812a6ea45c380d22
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-10-17 11:32:29 +0300

    Update bucket_sort.c

--- bucket_sort.c
+++ bucket_sort.c
@@ -1,60 +1,8 @@
-/*#include<stdlib.h>
-#include <stdio.h>
-
-struct dyn_2d
-{
-    unsigned element_size, x_dimension, y_dimension;
-    char data[];
-};
-
-struct dyn_2d *dyn_2d_create(unsigned element_size, unsigned x_dimension, unsigned y_dimension)
-{
-    struct dyn_2d *arr_data = malloc(sizeof *arr_data + element_size*x_dimension*y_dimension);
-    arr_data->element_size = element_size;
-    arr_data->x_dimension = x_dimension;
-    arr_data->y_dimension = y_dimension;
-    return arr_data;
-}
-
-void *dyn_2d_access(struct dyn_2d *a, unsigned x, unsigned y)
-{
-    return a->data + (y*a->x_dimension + x) * a->element_size;
-}
-
-void dyn_2d_destroy(struct dyn_2d *a)
-{
-    free(a);
-}
-
-//-------------------- generic code ends here -----------------------
-
-#define TYPE double
-
-int main()
-{
-    unsigned x_d = 11, y_d = 11;
-    struct dyn_2d *a = dyn_2d_create(sizeof(TYPE), x_d, y_d);
-    for (int y = 0; y < y_d; y++)
-        for (int x = 0; x < x_d; x++)
-            *(TYPE *) dyn_2d_access(a, x, y) = x * y;
-
-    for(int y = 0;y < y_d;y++)
-        for(int x = 0;x < x_d;x++)
-        {
-            TYPE *b = dyn_2d_access(a, x, y);
-            printf("%f\n", *b);
-        }
-
-    dyn_2d_destroy(a);
-    return 0;
-}*/
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
-#define SIZE 12
+#define SIZE 13
 
 int find_max(int arr[], int n)
 {
@@ -76,11 +24,10 @@ void bucket_sort(int arr[], int n)
     int buckets = 10;
     int divider =  ceil((max + 1) / buckets) + 2;
 
-    int *B = malloc(sizeof((*B)*buckets*divider));
+    int *B = malloc(sizeof(*B)*buckets*divider);
     int *B_i = malloc(sizeof(*B_i)*buckets);
 
-    if(!B || !B_i)
-        return;
+    if(!B || !B_i) return;
 
     for(int i = 0;i < buckets;B_i[i++] = 0);
 
@@ -96,19 +43,15 @@ void bucket_sort(int arr[], int n)
 
     int index = 0;
     for(int i = 0;i < buckets;i++)
-    {
         for(int j = 0;j < B_i[i];j++)
-        {
-            arr[index++] = B[i*divider+j];
-        }
-    }
+            printf("%d ", B[i*divider+j]);
+    free(B);
+    free(B_i);
 }
 
 int main()
 {
-    int a[] = {22, 45, 12, 8, 10, 6, 72, 81, 33, 18, 50, 14};
+    int a[] = {22, 45, 12, 8, 10, 6, 72, 81, 33, 18, 50, 14, 81};
     bucket_sort(a, SIZE);
-
-
     return 0;
 }
