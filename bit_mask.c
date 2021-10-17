ac5155bbe016fffdbe2fa6e7a817834e0a5dad64
fedarrdi <42972410+fedarrdi@users.noreply.github.com>
2021-10-17 14:23:33 +0300

    Update bit_mask.c

--- bit_mask.c
+++ bit_mask.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
-///mrejov maska
-
 unsigned long long pow(int base, int power)
 {
-    unsigned long long out = 1;
+    unsigned long long  out = 1;
     for(int i = 0;i < power;i++)
         out *= base;
     return out;
 }
 
-int n_of_bits(unsigned long long number)
+unsigned long long n_of_bits(unsigned long  number)
 {
-    int a, b;
-    for(int i = 0;i < 32;i++)
+    for(int i = 0;i <= 32;i++)
     {
-        b = pow(2, i);
-        a = number - b;
+        long long b = pow(2, i), a = number - b;
         if(a <= 0) return i;
     }
 }
 
-unsigned long long get_net_size(unsigned long long  mask)
+unsigned long long get_net_size(unsigned long mask)
 {
-    unsigned long long biggest_mask = pow(2, 32) - 1;
-    unsigned long long number = biggest_mask ^ mask;
+    unsigned long  biggest_mask = pow(2, 32) - 1;
+    unsigned long  number = biggest_mask ^ mask;
     short number_of_bits = n_of_bits(number);
-    int n_of_addresses = pow(2, number_of_bits);
-    return n_of_addresses - 2;
+    unsigned long long n_of_addresses = pow(2, number_of_bits);
+    return n_of_addresses < 2  ? n_of_addresses : n_of_addresses - 2;
 }
 
 int main()
 {
-    unsigned long long in = pow(2, 32) - 8;
-    unsigned long long a = get_net_size(in);
-
+    unsigned long long  in = pow(2, 32) - 15;
+    unsigned long  a = get_net_size(in);
     return 0;
 }
